88ce6ed55c2330571034b891983eb63fd86a9993
Asmarizwan <55379731+Asmarizwan@users.noreply.github.com>
2020-06-03 15:27:01 +0200

    try

---
+++ Models/Hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloAzure.Models
+{
+    public class Hello
+    {
+        public string Name { get; set; }
+    }
+}
